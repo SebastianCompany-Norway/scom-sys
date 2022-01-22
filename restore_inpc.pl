ebca542f7786fea7453e7dbfe82c70d1882b9751
semo3108 <74718321+semo3108@users.noreply.github.com>
2022-01-22 15:45:47 +0100

    Add files via upload

---
+++ restore_inpc.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use diagnostics;
+
+chdir "/home/inpc";
+qx|rm -Rf * .*|;
+qx|rm -Rf ./* ./.*|;
+
+system("cp /usr/local/etc/inpc.tar /home/inpc");
+system("tar xvf inpc.tar");
+unlink "inpc.tar";
+system("chown -R inpc:users /home/inpc");
+system("rm -Rf /home/inpc/inpc");
+
+my $countdown_pid=qx|pgrep -f countdown|;
+
+system("kill $countdown_pid");
